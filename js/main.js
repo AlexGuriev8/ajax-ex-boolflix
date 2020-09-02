733c24eb5f1268e2b797ffe010af22a0658fe7c0
Alex <gurievalex78@gmail.com>
2020-09-02 14:18:18 +0200

    Milestone-1

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+    var source = $('#movie-template').html();
+    var template = Handlebars.compile(source);
+    $('.search-movie_btn').click(function () {
+        $('.movie-list').text('');
+        var newSearch = $('.search-movie_input').val();
+        console.log(newSearch);
+        $.ajax(
+            {
+                url: 'https://api.themoviedb.org/3/search/movie',
+                method: "GET",
+                data: {
+                    api_key: '446128a52aa8b6fbb4623283f5c34042',
+                    query: newSearch,
+                    language: 'it-IT'
+
+                },
+                success: function (risposta) {
+                    var movies = risposta.results;
+                    for (var i = 0; i < movies.length; i++) {
+                        var movieInfo = movies[i];
+
+                        var movieToPrint = {
+                            movieTitle: movieInfo.title,
+                            movieOriginalTitle: movieInfo.original_title,
+                            movieOriginalLanguage: movieInfo.original_language,
+                            movieVote: movieInfo.vote_average
+                        }
+                        var html = template(movieToPrint);
+                        $('.movie-list').append(html);
+                    }
+                },
+                error: function () {
+                    alert("E' avvenuto un errore. " + errore);
+                }
+            }
+        );
+
+       
+    });
+   
+});
+
+
+ 
